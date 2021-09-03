9b9fb24be6ea5bda692f2d03e5d39ebcaa89a7d2
maskho <maskho@users.noreply.github.com>
2021-09-03 17:39:43 +0000

    deploy: d2ec695e63ecda1cb4f088d349e1a547210e0592

---
+++ precache-manifest.45e9f42261dd9400473d587493d94dba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6130b796f930fea7467dda4b39c21d",
+    "url": "https://maskho.github.io/enpact-testing-frontend/index.html"
+  },
+  {
+    "revision": "c1a8378ac1235534e558",
+    "url": "https://maskho.github.io/enpact-testing-frontend/static/css/main.0f5f634e.chunk.css"
+  },
+  {
+    "revision": "dd3bc212f1072f0fdd51",
+    "url": "https://maskho.github.io/enpact-testing-frontend/static/js/2.c6f87a8d.chunk.js"
+  },
+  {
+    "revision": "c1a8378ac1235534e558",
+    "url": "https://maskho.github.io/enpact-testing-frontend/static/js/main.ac4259a4.chunk.js"
+  },
+  {
+    "revision": "b2fd83ba2c9f4d957d0e",
+    "url": "https://maskho.github.io/enpact-testing-frontend/static/js/runtime~main.fc212185.js"
+  }
+]);
